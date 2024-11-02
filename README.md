966ab338c9820a5520291eb74056bac285a7607b
Martin Manjoro <martin.manjoro25@gmail.com>
2024-11-02 17:22:11 +0200

    Changes
    reset the whole project coz of errors

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-chat-service
-Django service that handles messaging and chats
--- chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import send_message, get_messages
-
-urlpatterns = [
-    path('send/', send_message, name='send_message'),
-    path('messages/<str:recipient>/', get_messages, name='get_messages'),
-]
